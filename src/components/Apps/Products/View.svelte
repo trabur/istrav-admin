96957b1a2821417fe6f23bdb08ccba7f588c0c0a
trabur <travis.burandt@gmal.com>
2021-02-10 16:54:31 -0600

    collections categories products

---
+++ src/components/Apps/Products/View.svelte
@@ -0,0 +1,65 @@
+<script>
+  export let domain
+  export let state
+</script>
+
+<h3 class="title">
+  <a href={`/apps/${domain}/${state}`}><i class="material-icons">store</i> {domain}/products</a>
+  <div class="right">
+    <i class="material-icons">flag</i> {state}
+  </div>
+</h3>
+
+<a href={`/apps/${domain}/${state}/products/add`} class="floating-add btn-floating btn-large waves-effect waves-light"><i class="material-icons">add</i></a>
+
+<div class="list">
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Item Name</th>
+        <th>Item Price</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr>
+        <td>Alvin</td>
+        <td>Eclair</td>
+        <td>$0.87</td>
+      </tr>
+      <tr>
+        <td>Alan</td>
+        <td>Jellybean</td>
+        <td>$3.76</td>
+      </tr>
+      <tr>
+        <td>Jonathan</td>
+        <td>Lollipop</td>
+        <td>$7.00</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<style>
+  .floating-add {
+    position: fixed;
+    right: 1em;
+    bottom: 1em;
+  }
+  
+  .title {
+    padding: 0.5em; 
+    font-size: 2rem;
+    font-weight: 900;
+    background: #333;
+    box-shadow: inset 0px 10px 10px -10px #000;
+    color: #eee;
+    margin: 0;
+  }
+
+  .list {
+    margin: 0 1em;
+  }
+</style>
---
+++ src/routes/apps/[domain]/[state]/categories.svelte
@@ -0,0 +1,31 @@
+
+<script>
+  import { onMount } from 'svelte';
+  import View from '../../../../components/Apps/Categories/View.svelte'
+  import Navigation from '../../../../components/Header/Navigation.svelte'
+  import MainLinks from '../../../../components/Header/MainLinks.svelte'
+
+  import { stores } from "@sapper/app"
+  const { page } = stores()
+
+  // When this is true, show the component
+  let load = false
+  let domain
+  let state
+
+  $: { reMount($page.params.domain) }
+  $: { reMount($page.params.state) }
+  function reMount() {
+    load = false
+    setTimeout(() => load = true, 0)
+    domain = $page.params.domain
+    state = $page.params.state
+  }
+</script>
+
+<Navigation>
+  <MainLinks domain={domain} state={state} />
+</Navigation>
+{#if load === true}
+  <View domain={domain} state={state}  />
+{/if}
