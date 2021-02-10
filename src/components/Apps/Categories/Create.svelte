5c797ed247fa5ababb9445a431c927774880e8f4
trabur <travis.burandt@gmal.com>
2021-02-10 17:38:27 -0600

    collections categories products 2

---
+++ src/components/Apps/Categories/Create.svelte
@@ -0,0 +1,65 @@
+
+<script>
+  import { onMount } from 'svelte';
+
+	export let domain = '';
+  export let state = '';
+
+  let name = ''
+  let slug = ''
+  let image = ''
+
+	async function create() {
+    if (name === '') return alert('Name must be defined.')
+    if (slug === '') return alert('Slug must be defined.')
+    if (image === '') return alert('Image must be defined.')
+
+    let token = localStorage.getItem('token')
+    let change = {
+      name,
+      slug,
+      image
+    }
+    let esSave = await scripts.store.categories.getSave(token, change)
+    console.log('esSave', esSave)
+    if (esSave.payload.success === true) {
+      window.location = `/apps/${domain}/${state}/categories`
+    } else {
+      alert(esSave.payload.reason)
+    }
+  }
+</script>
+
+<div class="row">
+  <div class="col s12 m4"></div>
+  <div class="col s12 m4">
+    <h3 class="title">CREATE CATEGORY</h3>
+    <div class="card" style="padding: 1em; background: #eee;">
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="name" type="text" class="validate" bind:value={name}>
+          <label for="name">Name</label>
+        </div>
+        <div class="input-field col s12">
+          <input id="slug" type="text" class="validate" bind:value={slug}>
+          <label for="slug">Slug</label>
+        </div>
+        <br />
+        <button style="margin-left: 1em;" type='submit' class="waves-effect btn" on:click={() => create()}>Submit</button>
+      </div>
+    </div>
+    <div style="text-align: right;">
+      <a href={`/apps/${domain}/${state}/categories`} class="waves-effect red lighten-2 btn">Cancel</a>
+    </div>
+  </div>
+  <div class="col s12 m4"></div>
+</div>
+
+<style>
+  .title {
+    margin: 0; 
+    text-align: center;
+    font-size: 2rem;
+    font-weight: 900;
+  }
+</style>
---
+++ src/routes/apps/[domain]/[state]/products/add.svelte
@@ -0,0 +1,34 @@
+<svelte:head>
+	<title>CREATE ::: ISTRAV</title>
+</svelte:head>
+
+<script>
+  import { onMount } from 'svelte'
+  import Create from '../../../../../components/Apps/Products/Create.svelte'
+  import Navigation from '../../../../../components/Header/Navigation.svelte'
+  
+  import { stores } from "@sapper/app"
+  const { page } = stores()
+
+  // When this is true, show the component
+  let load = false
+  let domain
+  let state
+
+  $: { reMount($page.params.domain) }
+  $: { reMount($page.params.state) }
+  function reMount() {
+    load = false
+    setTimeout(() => load = true, 0)
+    domain = $page.params.domain
+    state = $page.params.state
+  }
+</script>
+
+<Navigation />
+<br />
+<br />
+<br />
+{#if load}
+  <Create domain={domain} state={state} />
+{/if}
