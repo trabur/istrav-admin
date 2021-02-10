407a661141ab8f8bfa4e07c185ca561a80cb667b
trabur <travis.burandt@gmal.com>
2021-02-10 15:33:47 -0600

    boxes

--- src/components/Header/MainLinks.svelte
+++ src/components/Header/MainLinks.svelte
@@ -3,8 +3,8 @@
   export let state
 </script>
 <ul class="right">
-  <li><a href={`/apps/${domain}/${state}/featured`}><i class="material-icons">star</i></a></li>
+  <li><a href={`/apps/${domain}/${state}/collections`}><i class="material-icons">apps</i></a></li>
   <li><a href={`/apps/${domain}/${state}/categories`}><i class="material-icons">widgets</i></a></li>
-  <li><a href={`/apps/${domain}/${state}/products`}><i class="material-icons">shopping_basket</i></a></li>
+  <li><a href={`/apps/${domain}/${state}/products`}><i class="material-icons">stop</i></a></li>
   <li><a href={`/apps/${domain}/${state}/navigations`}><i class="material-icons">format_list_numbered</i></a></li>
 </ul>
