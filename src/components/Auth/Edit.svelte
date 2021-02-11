9ecf887bb909b2c1406efd59db4f6259fae8b6c9
trabur <travis.burandt@gmal.com>
2021-02-10 19:34:48 -0600

    edit

---
+++ src/components/Auth/Edit.svelte
@@ -0,0 +1,101 @@
+<script>
+  import { onMount, onDestroy } from 'svelte';
+  // import { login, register } from './methods'
+  import parseJwt from '../../helpers/parseJwt.js'
+
+  let token
+  let decoded
+	let email = '';
+  let password = '';
+  let passwordRepeat = '';
+  let username = '';
+  let firstName = '';
+  let lastName = '';
+
+  onMount(async () => {
+    M.updateTextFields();
+
+    token = localStorage.getItem('token')
+    decoded = parseJwt(token)
+    console.log('decoded', decoded)
+
+    let esOne = await scripts.tenant.members.getOne(decoded.email)
+    console.log('esOne', esOne)
+    if (esOne.payload.success === true) {
+      let member = esOne.payload.data
+      console.log('member', member)
+      email = decoded.email
+      username = member.username
+      firstName = member.firstName
+      lastName = member.lastName
+      setTimeout(() => M.updateTextFields(), 0)
+    } else {
+      alert(esOne.payload.reason)
+    }
+  })
+
+	async function submit() {
+    if (email === '') return alert('Email must be defined.')
+    if (username === '') return alert('Username must be defined.')
+    if (firstName === '') return alert('First name must be defined.')
+    if (lastName === '') return alert('Last name must be defined.')
+
+    let change = {
+      email,
+      username,
+      firstName,
+      lastName
+    }
+
+    let getUpdate = await scripts.tenant.members.getUpdate(token, decoded.email, change)
+    console.log('getUpdate', getUpdate)
+
+    if (getUpdate.payload.success === true) {
+      window.location = '/apps'
+    } else {
+      alert(getUpdate.payload.reason)
+    }
+  }
+</script>
+
+<div class="row">
+  <div class="col s12 m4"></div>
+  <div class="col s12 m4">
+    <h3 class="title">MY PROFILE</h3>
+    <div class="card" style="padding: 1em; background: #eee;">
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="text" class="validate" bind:value={email}>
+          <label for="email">Email</label>
+        </div>
+        <div class="input-field col s12">
+          <input id="username" type="text" class="validate" bind:value={username}>
+          <label for="username">Username</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="firstName" type="text" class="validate" bind:value={firstName}>
+          <label for="firstName">First Name</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="lastName" type="text" class="validate" bind:value={lastName}>
+          <label for="lastName">Last Name</label>
+        </div>
+        <br />
+        <button style="margin-left: 1em;" type='submit' class="waves-effect btn" on:click={() => submit()}>Submit</button>
+      </div>
+    </div>
+    <div style="text-align: right;">
+      <a href="/apps" class="waves-effect red lighten-2 btn">APPS</a>
+    </div>
+  </div>
+  <div class="col s12 m4"></div>
+</div>
+
+<style>
+.title {
+  margin: 0; 
+  text-align: center;
+  font-size: 2rem;
+  font-weight: 900;
+}
+</style>
---
+++ src/routes/edit.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+	<title>ADMIN ::: ISTRAV</title>
+</svelte:head>
+
+<script>
+  import { onMount } from 'svelte';
+	import Edit from '../components/Auth/Edit.svelte'
+	import Navigation from '../components/Header/Navigation.svelte'
+</script>
+
+<Navigation />
+<br />
+<br />
+<br />
+<Edit />
