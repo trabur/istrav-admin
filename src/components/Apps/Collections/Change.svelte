897107757e7af3ff959aa79ecd3b25f132579be3
trabur <travis.burandt@gmal.com>
2021-02-12 17:23:08 -0600

    change

---
+++ src/components/Apps/Collections/Change.svelte
@@ -0,0 +1,93 @@
+
+<script>
+  import { onMount } from 'svelte';
+
+	export let domain = '';
+  export let state = '';
+  export let slugId = '';
+
+  let name = ''
+  let slug = slugId
+  let image = ''
+  let appId
+
+	async function change() {
+    if (name === '') return alert('Name must be defined.')
+    if (slug === '') return alert('Slug must be defined.')
+
+    let token = localStorage.getItem('token')
+    let change = {
+      name,
+      slug,
+      image
+    }
+    let esSave = await scripts.store.collections.getUpdate(appId, token, slugId, change)
+    console.log('esSave', esSave)
+    if (esSave.payload.success === true) {
+      window.location = `/apps/${domain}/${state}/collections`
+    } else {
+      alert(esSave.payload.reason)
+    }
+  }
+
+  onMount(async () => {
+    M.updateTextFields();
+
+    let esOne = await scripts.tenant.apps.getOne(domain, state)
+    console.log('esOne', esOne)
+    if (esOne.payload.success === true) {
+      appId = esOne.payload.data.id
+      let esCollection = await scripts.store.collections.getOne(appId, slug)
+      console.log('esCollection', esCollection)
+      if (esCollection.payload.success === true) {
+        let data = esCollection.payload.data
+        name = data.name
+        slug = data.slug
+        image = data.image
+        setTimeout(() => M.updateTextFields(), 0)
+      } else {
+        alert(esCollection.payload.reason)
+      }
+    } else {
+      alert(esOne.payload.reason)
+    }
+  })
+</script>
+
+<div class="row">
+  <div class="col s12 m4"></div>
+  <div class="col s12 m4">
+    <h3 class="title">CHANGE COLLECTION</h3>
+    <div class="card" style="padding: 1em; background: #eee;">
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="name" type="text" class="validate" bind:value={name}>
+          <label for="name">Name</label>
+        </div>
+        <div class="input-field col s12">
+          <input id="slug" type="text" class="validate" bind:value={slug}>
+          <label for="slug">Slug</label>
+        </div>
+        <div class="input-field col s12">
+          <input id="image" type="text" class="validate" bind:value={image}>
+          <label for="image">Image</label>
+        </div>
+        <br />
+        <button style="margin-left: 1em;" type='submit' class="waves-effect btn" on:click={() => change()}>Submit</button>
+      </div>
+    </div>
+    <div style="text-align: right;">
+      <a href={`/apps/${domain}/${state}/collections`} class="waves-effect red lighten-2 btn">COLLECTIONS</a>
+    </div>
+  </div>
+  <div class="col s12 m4"></div>
+</div>
+
+<style>
+  .title {
+    margin: 0; 
+    text-align: center;
+    font-size: 2rem;
+    font-weight: 900;
+  }
+</style>
---
+++ src/routes/apps/[domain]/[state]/products/[slug]/index.svelte
@@ -0,0 +1,40 @@
+<svelte:head>
+	<title>CREATE ::: ISTRAV</title>
+</svelte:head>
+
+<script>
+  import { onMount } from 'svelte'
+  import Change from '../../../../../../components/Apps/Products/Change.svelte'
+  import Navigation from '../../../../../../components/Header/Navigation.svelte'
+  import MainLinks from '../../../../../../components/Header/MainLinks.svelte'
+  
+  import { stores } from "@sapper/app"
+  const { page } = stores()
+
+  // When this is true, show the component
+  let load = false
+  let domain
+  let state
+  let slug
+
+  $: { reMount($page.params.domain) }
+  $: { reMount($page.params.state) }
+  $: { reMount($page.params.slug) }
+  function reMount() {
+    load = false
+    setTimeout(() => load = true, 0)
+    domain = $page.params.domain
+    state = $page.params.state
+    slug = $page.params.slug
+  }
+</script>
+
+<Navigation>
+  <MainLinks domain={domain} state={state} />
+</Navigation>
+<br />
+<br />
+<br />
+{#if load}
+  <Change domain={domain} state={state} slugId={slug}  />
+{/if}
