6b9c4d047361c422f63473cee5437e2a3edc1a8d
trabur <travis.burandt@gmal.com>
2021-02-12 18:45:31 -0600

    ccp

--- src/components/Apps/Totals.svelte
+++ src/components/Apps/Totals.svelte
@@ -7,8 +7,8 @@
 
 {#if totals}
   <div class="masonry">
-    <a href={`/apps/${domain}/${state}/categories`} class="waves-effect btn red lighten-2 left">categories ({totals.categoriesCount})</a>
     <a href={`/apps/${domain}/${state}/collections`} class="waves-effect btn red lighten-2 left">collections ({totals.collectionsCount})</a>
+    <a href={`/apps/${domain}/${state}/categories`} class="waves-effect btn red lighten-2 left">categories ({totals.categoriesCount})</a>
     <a href={`/apps/${domain}/${state}/products`} class="waves-effect btn red lighten-2 left">products ({totals.productsCount})</a>
   </div>
 {/if}
