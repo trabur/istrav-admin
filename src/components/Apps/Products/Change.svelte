b80989984c444c968503c3d3963a8fe0ae45d38c
trabur <travis.burandt@gmal.com>
2021-02-13 16:15:29 -0600

    choices

--- src/components/Apps/Products/Change.svelte
+++ src/components/Apps/Products/Change.svelte
@@ -1,6 +1,6 @@
-
 <script>
   import { onMount } from 'svelte';
+  import { each } from 'svelte/internal';
 
 	export let domain = '';
   export let state = '';
@@ -10,21 +10,27 @@
   let slug = slugId
   let image = ''
   let appId
+  let categoryId = ''
+  let categories = []
+  let categoryIdChoices
 
 	async function change() {
     if (name === '') return alert('Name must be defined.')
     if (slug === '') return alert('Slug must be defined.')
+    categoryId = categoryIdChoices.getValue(true)
+    console.log('categoryId', categoryId)
 
     let token = localStorage.getItem('token')
     let change = {
       name,
       slug,
-      image
+      image,
+      categoryId
     }
     let esSave = await scripts.store.products.getUpdate(appId, token, slugId, change)
     console.log('esSave', esSave)
     if (esSave.payload.success === true) {
-      window.location = `/apps/${domain}/${state}/products`
+      // window.location = `/apps/${domain}/${state}/products`
     } else {
       alert(esSave.payload.reason)
     }
@@ -37,6 +43,8 @@
     console.log('esOne', esOne)
     if (esOne.payload.success === true) {
       appId = esOne.payload.data.id
+
+      // fetch product
       let esProduct = await scripts.store.products.getOne(appId, slug)
       console.log('esProduct', esProduct)
       if (esProduct.payload.success === true) {
@@ -44,10 +52,35 @@
         name = data.name
         slug = data.slug
         image = data.image
+        categoryId = data.categoryId
         setTimeout(() => M.updateTextFields(), 0)
       } else {
         alert(esProduct.payload.reason)
       }
+
+      // fetch categories for dropdown
+      let esCategories = await scripts.store.categories.getAll(appId)
+      console.log('esCategories', esCategories)
+      if (esCategories.payload.success === true) {
+        categories = esCategories.payload.data
+      } else {
+        alert(esCategories.payload.reason)
+      }
+      
+      setTimeout(() => {
+        const categoryIdElement = document.querySelector('#categoryId');
+        categoryIdChoices = new Choices(categoryIdElement);
+        categories.forEach((value, index) => {
+          console.log(`${value.id} === ${categoryId}`)
+          if (value.id === categoryId) {
+            categories[index].selected = true
+          }
+          categories[index].value = value.id
+          categories[index].label = value.name
+        })
+        console.log('categories', categories)
+        categoryIdChoices.setChoices(categories, 'value', 'label', false)
+      }, 0)
     } else {
       alert(esOne.payload.reason)
     }
@@ -72,7 +105,16 @@
           <input id="image" type="text" class="validate" bind:value={image}>
           <label for="image">Image</label>
         </div>
-        <br />
+        <div class="input-field col s12">
+          {#if categories.length}
+            <div class="label">Category</div>
+            <div class="choices">
+              <select id="categoryId" class="choices" bind:value={categoryId}></select>
+            </div>
+            <br />
+            <br />
+          {/if}
+        </div>
         <button style="margin-left: 1em;" type='submit' class="waves-effect btn" on:click={() => change()}>Submit</button>
       </div>
     </div>
@@ -89,5 +131,18 @@
     text-align: center;
     font-size: 2rem;
     font-weight: 900;
+  }
+
+  .choices {
+    position: absolute;
+    right: 0.75em;
+    left: 0.75em;
+    z-index: 10;
+  }
+
+  .label {
+    color: #9E9E99;
+    font-size: 0.7em;
+    margin-top: -1em;
   }
 </style>
