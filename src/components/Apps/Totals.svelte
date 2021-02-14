7ff6c043aaed66ee46e657f32c2e2aab5a2a401b
trabur <travis.burandt@gmal.com>
2021-02-14 13:26:59 -0600

    menus pages orders

--- src/components/Apps/Totals.svelte
+++ src/components/Apps/Totals.svelte
@@ -11,6 +11,7 @@
     <a href={`/apps/${domain}/${state}/categories`} class="waves-effect btn red lighten-2 left">categories ({totals.categoriesCount})</a>
     <a href={`/apps/${domain}/${state}/products`} class="waves-effect btn red lighten-2 left">products ({totals.productsCount})</a>
     <a href={`/apps/${domain}/${state}/menus`} class="waves-effect btn red lighten-2 left">menus ({totals.menusCount})</a>
+    <a href={`/apps/${domain}/${state}/pages`} class="waves-effect btn red lighten-2 left">pages ({totals.pagesCount})</a>
     <a href={`/apps/${domain}/${state}/users`} class="waves-effect btn red lighten-2 left">users ({totals.usersCount})</a>
     <a href={`/apps/${domain}/${state}/orders`} class="waves-effect btn red lighten-2 left">orders ({totals.ordersCount})</a>
   </div>
--- src/components/Header/MainLinks.svelte
+++ src/components/Header/MainLinks.svelte
@@ -7,6 +7,7 @@
   <li><a href={`/apps/${domain}/${state}/categories`}><i class="material-icons">widgets</i></a></li>
   <li><a href={`/apps/${domain}/${state}/products`}><i class="material-icons">stop</i></a></li>
   <li><a href={`/apps/${domain}/${state}/menus`}><i class="material-icons">format_list_numbered</i></a></li>
+  <li><a href={`/apps/${domain}/${state}/pages`}><i class="material-icons">note</i></a></li>
   <li><a href={`/apps/${domain}/${state}/users`}><i class="material-icons">person</i></a></li>
   <li><a href={`/apps/${domain}/${state}/orders`}><i class="material-icons">markunread_mailbox</i></a></li>
 </ul>
