aec1ad2294a84395565cc3c6f2bb9e4d25ceb92e
istrav <travis.burandt@gmail.com>
2021-03-20 17:11:03 -0500

    in

--- src/components/Auth/Welcome.svelte
+++ src/components/Auth/Welcome.svelte
@@ -3,7 +3,7 @@
     <h1 class="logo">METAHEAP.IO</h1>
     <div class="power">Powered by <a href="https://istrav.com">ISTRAV</a></div>
     <br />
-    <div class="desc">Welcome, <a href="https://metaheap.io">metaheap.io</a> is the admin control panel for creating and managing eCommerce storefronts built for website owners. Checkout our list of live examples at <a href="https://dimension.click">dimension.click</a> for a demonstration of what can be achieved. Getting started is FREE; within minutes, be up and running with a production ready shop in just a few clicks.</div>
+    <div class="desc">Welcome, <a href="https://metaheap.io">metaheap.io</a> is the admin control panel for creating and managing eCommerce storefronts built for website owners. Checkout our list of live examples at <a href="https://dimension.click">dimension.click</a> for a demonstration of what can be achieved. Getting started is FREE; in minutes, be up and running with a production ready shop in just a few clicks.</div>
     <br />
     <a href="https://docs.istrav.com" target="_blank" class="waves-effect btn">DOCUMENTATION</a>
   </div>
