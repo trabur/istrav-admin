21e4fd99a6118de520701b04632c0a33bae36217
istrav <travis.burandt@gmail.com>
2021-03-26 13:59:01 -0500

    Web Solutions

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,10 +3,10 @@
     <div class="row">
       <div class="col l6 s12">
         <h5 class="white-text">METAHEAP.IO</h5>
-        <p class="grey-text text-lighten-4">The admin control panel for creating and managing eCommerce solutions built for website owners.</p>
+        <p class="grey-text text-lighten-4">The admin control panel for creating and managing web solutions built for website owners.</p>
       </div>
       <div class="col l4 offset-l2 s12">
-        <h5 class="white-text">eCommerce Solutions</h5>
+        <h5 class="white-text">Web Solutions</h5>
         <ul>
           <li><a class="grey-text text-lighten-3" href="#!">Marketing</a></li>
           <li><a class="grey-text text-lighten-3" href="#!">Storefronts</a></li>
