ab1018455cedbc8686844750d3daa66c21236324
istrav <travis.burandt@gmail.com>
2021-03-27 17:54:44 -0500

    footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,25 +2,24 @@
   <div class="container">
     <div class="row">
       <div class="col l6 s12">
-        <h5 class="white-text">METAHEAP.IO</h5>
-        <p class="grey-text text-lighten-4">The admin control panel for creating and managing web solutions built for website owners.</p>
+        <h5 class="white-text">ISTRAV</h5>
+        <p class="grey-text text-lighten-4">Complete custom & white label web solutions. Includes admin control panel (metaheap.io). Built headless (farmerless.com) with CQRS in mind so you can detach any part of the system and roll your own at any time. Underneath the hood (hacktracks.org) our backend REST API is "event source" driven; the same system used by banks to keep track of records. We are doing some amazing things around here at ISTRAV, so stick around, and feel free to give us feeback along the way.</p>
       </div>
       <div class="col l4 offset-l2 s12">
         <h5 class="white-text">Web Solutions</h5>
         <ul>
-          <li><a class="grey-text text-lighten-3" href="#!">Marketing</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Storefronts</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Blogs</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Forum</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Promo</a></li>
+          <li><a class="grey-text text-lighten-3" href="https://tyu67.com">Marketing</a></li>
+          <li><a class="grey-text text-lighten-3" href="https://dimension.click">Storefronts</a></li>
+          <li><a class="grey-text text-lighten-3" href="https://burnfort.com">Blogs</a></li>
+          <li><a class="grey-text text-lighten-3" href="https://aaghc.com">Forums</a></li>
+          <li><a class="grey-text text-lighten-3" href="https://printedbasics.com">Promo</a></li>
         </ul>
       </div>
     </div>
   </div>
   <div class="footer-copyright">
     <div class="container">
-      Copyright @{new Date().getFullYear()} <a href="https://metaheap.io">{`METAHEAP.IO`}</a>. All Rights Reserved.
-      <div class="right">Powered by <a href="https://istrav.com" target="_blank">ISTRAV</a>.</div>
+      Copyright @{new Date().getFullYear()} <a href="https://metaheap.io">{`METAHEAP.IO`}</a>. All Rights Reserved. Powered by <a href="https://istrav.com" target="_blank">ISTRAV</a>.
     </div>
   </div>
 </footer>
