83b5bcdcb5ee3257aae4cd7c56dadff436e974cb
istrav <travis.burandt@gmail.com>
2021-04-24 12:36:42 -0500

    products table

--- src/components/Apps/Products/View.svelte
+++ src/components/Apps/Products/View.svelte
@@ -1,8 +1,18 @@
 <script>
   import { onMount } from 'svelte';
 
+  import { Datatable, rows } from 'svelte-simple-datatables'
+
+  const settings = { 
+    columnFilter: true,
+    rowPerPage: 15,
+    scrollY: false,
+    css: false // we use static/table.css instead
+  }
+
   export let domain
   export let state
+  
   let appId
   let endpoint
   let table = []
@@ -41,19 +51,18 @@
       <a href={`/apps/${domain}/${state}/products/add`} class="floating-add btn-floating btn-large waves-effect waves-light"><i class="material-icons">add</i></a>
       <div class="list">
         {#if table.length}
-          <table>
+          <Datatable settings={settings} data={table}>
             <thead>
               <tr>
-                <th>Image</th>
-                <th>Name</th>
-                <th>Slug</th>
-                <th>Price</th>
-                <th style="text-align: right;">Change</th>
+                <th data-key="image">Image</th>
+                <th data-key="name">Name</th>
+                <th data-key="slug">Slug</th>
+                <th data-key="price">Price</th>
+                <th data-key="id" style="text-align: right;">Change</th>
               </tr>
             </thead>
-
             <tbody>
-              {#each table as row (row.id)}
+              {#each $rows as row}
                 <tr>
                   <td>
                     <img src={`${uploads}/${row.image}`} class="image" alt={row.image} />
@@ -65,7 +74,7 @@
                 </tr>
               {/each}
             </tbody>
-          </table>
+          </Datatable>
         {/if}
       </div>
     </div>
@@ -87,7 +96,8 @@
   }
 
   .list {
-    margin: 0 1em;
+    /* margin: 1em 0;
+    padding: 1em 0; */
   }
 
   .list .image {
