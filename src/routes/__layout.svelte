cddd0cc5752c1afbd440f29f3fc38525d1bfcf45
istrav <travis.burandt@gmail.com>
2021-05-29 16:12:12 -0500

    configure backend

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -17,41 +17,75 @@
 <div id="code"></div>
 
 <script>
-  import { onMount, onDestroy } from 'svelte';
-  
-  import '../../static/global.css'
-  import '../../static/table.css'
+  import { onMount } from 'svelte';
+	import { appData, domainId, backendId, headlessId, ioId } from '../stores.js';
+
+  import { istrav, scripts } from '../../farmerless/api'
 
   export let apiUri
   export let ioUri
   export let headlessUri
 
-  let domainId
+  let app
+  appData.subscribe(value => {
+		app = value
+	})
+  let domain
+  domainId.subscribe(value => {
+		domain = value
+	})
   let backend
+  backendId.subscribe(value => {
+		backend = value
+	})
   let headless
+  headlessId.subscribe(value => {
+    headless = value
+	})
   let io
+  ioId.subscribe(value => {
+    io = value
+	})
 
-  let loading = false
+  let loading = true
+  let state = 'production'
 
-  onMount(() => {
-    // appId domain state
+  onMount(async () => {
+    // setup links
     if (window.location.host === 'localhost:5280') {
-      domainId = 'istrav.com'
-      headless = 'http://localhost:9999'
-      backend = 'http://localhost:1337'
-      io = 'http://localhost:3333'
-      console.log('localhost settings:', domainId, backend, headless, io)
+      console.log('using localhost settings:')
+      domainId.set('istrav.com')
+      headlessId.set('http://localhost:9999')
+      backendId.set('http://localhost:1337')
+      ioId.set('http://localhost:3333')
     } else {
-      domainId = window.location.host
-      headless = headlessUri || 'https://farmerless.com'
-      backend = apiUri || 'https://api.hacktracks.org'
-      io = ioUri || 'https://io.hacktracks.org'
-      console.log('production settings:', domainId, backend, headless, io)
+      console.log('using production settings:')
+      domainId.set(window.location.host)
+      headlessId.set(headlessUri || 'https://farmerless.com')
+      backendId.set(apiUri || 'https://api.hacktracks.org')
+      ioId.set(ioUri || 'https://io.hacktracks.org')
     }
+    console.log(domain, backend, headless, io)
+    
+    // configure backend
+    istrav.account.carts.init({ host: backend })
+    istrav.account.orders.init({ host: backend })
+    istrav.account.users.init({ host: backend })
+    istrav.app.blocks.init({ host: backend })
+    istrav.app.faq.init({ host: backend })
+    istrav.app.menus.init({ host: backend })
+    istrav.app.pages.init({ host: backend })
+    istrav.channel.guides.init({ host: backend })
+    istrav.channel.playlists.init({ host: backend })
+    istrav.channel.videos.init({ host: backend })
+    istrav.store.categories.init({ host: backend })
+    istrav.store.collections.init({ host: backend })
+    istrav.store.products.init({ host: backend })
+    istrav.subscription.licenses.init({ host: backend })
+    istrav.subscription.plans.init({ host: backend })
+    istrav.tenant.apps.init({ host: backend })
+    istrav.tenant.members.init({ host: backend })
 
-    // expose vars for use within app
-    // window['backend'] = backend
-    // window['io'] = io
-    // window['headless'] = headless
+    loading = false
   })
 </script>
